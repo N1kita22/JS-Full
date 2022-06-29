10393133898354f8a135cdb7e028f6cf8b9ba166
N1kita22 <nyxboxemail@gmail.com>
2022-06-29 16:30:14 +0300

    task9

---
+++ lesson7/09/script.js
@@ -0,0 +1,23 @@
+const withdraw = (clients, balances, client, amount) => {
+  const currentClient = clients.indexOf(client);
+  console.log(currentClient);
+  return balances[currentClient] > amount ? (balances[currentClient] -= amount) : -1;
+};
+
+//example 1:
+
+//input
+console.log(withdraw(['Ann', 'John', 'User'], [1400, 87, -6], 'John', 50));
+
+//output
+//37
+//и массив balances должен быть [1400, 37, -6]
+
+//example 2:
+
+//input
+console.log(withdraw(['Ann', 'John', 'User'], [1400, 87, -6], 'User', 10));
+
+//output
+//-1
+//и массив balances должен быть [1400, 87, -6]
