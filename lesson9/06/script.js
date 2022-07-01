48dd25435421b1dcf4d175f1858cebde8554bd9c
N1kita22 <nyxboxemail@gmail.com>
2022-07-01 22:15:07 +0300

    task6

---
+++ lesson9/06/script.js
@@ -0,0 +1,14 @@
+const getTotalRevenue = transactions =>
+  transactions.reduce((acc, amount) => {
+    return (acc += amount.amount);
+  }, 0);
+
+// examples
+const dayTransactions = [
+  { userId: 22, amount: 60, operation: 'sell' },
+  { userId: 22, amount: 160, operation: 'buy' },
+  { userId: 44, amount: 90, operation: 'sell' },
+];
+
+const result = getTotalRevenue(dayTransactions); // ==> 310
+console.log(result);
