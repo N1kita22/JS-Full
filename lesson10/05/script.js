f1d83f6c01d9a13b2f6793807dcb1f4f6abeebe2
N1kita22 <nyxboxemail@gmail.com>
2022-07-04 13:19:54 +0300

    bagfix

--- lesson10/05/script.js
+++ lesson10/05/script.js
@@ -1,4 +1,4 @@
 const arr1 = [12.23554, 13.6654467, 146.46654];
-const getTotalPrice = arr => Math.floor(arr.reduce((acc, el) => (acc += el), 0) * 100) / 100;
+const getTotalPrice = arr => '$' + Math.floor(arr.reduce((acc, el) => (acc += el), 0) * 100) / 100;
 
 console.log(getTotalPrice(arr1));
