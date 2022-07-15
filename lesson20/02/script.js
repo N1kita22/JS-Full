badff2a8ffecd92ccf706f945c695ee9c213de2c
N1kita22 <nyxboxemail@gmail.com>
2022-07-15 16:35:27 +0300

    task2

---
+++ lesson20/02/script.js
@@ -0,0 +1,31 @@
+export class Vehicle {
+  constructor(name, numberOfWheels) {
+    this.name = name;
+    this.numberOfWheels = numberOfWheels;
+  }
+
+  move() {
+    console.log(`${this.name} is moving`);
+  }
+
+  stop() {
+    console.log(`${this.name} stopped`);
+  }
+}
+
+export class Ship extends Vehicle {
+  constructor(name, numberOfWhells, maxSpeed) {
+    super(name, numberOfWhells);
+    this.maxSpeed = maxSpeed;
+  }
+
+  move() {
+    console.log(`${this.name} lifting anchor up`);
+    super.move();
+  }
+
+  stop() {
+    console.log(`${this.name} lifting anchor down`);
+    super.stop();
+  }
+}
