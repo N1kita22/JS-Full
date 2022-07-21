5e5ca248097c38e5be1cc8174a491599b12fdea0
N1kita22 <nyxboxemail@gmail.com>
2022-07-21 18:18:10 +0300

    task4

---
+++ lesson21/04/index.js
@@ -0,0 +1,19 @@
+export const getTitle = () => {
+  return document.querySelector('.title').textContent;
+};
+
+export const getDescription = () => {
+  return document.querySelector('.about').innerText;
+};
+
+export const getPlans = () => {
+  return document.querySelector('.plans').innerHTML;
+};
+
+export const getGoals = () => {
+  return document.querySelector('.goal').outerHTML;
+};
+console.log(getTitle());
+console.dir(getDescription());
+console.log(getPlans());
+console.log(getGoals());
